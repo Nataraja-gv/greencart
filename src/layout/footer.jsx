827d7a9b7c58717989fd26b9c3a42f4d4a63cbcf
Nataraja-gv <nataraja2k@gmail.com>
2025-05-07 16:33:07 +0530

    footer add margin

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-[#EAF4EE] text-[#1F2937]">
+    <div className="bg-[#EAF4EE] text-[#1F2937] mt-[30px]">
       <div className="max-w-7xl mx-auto px-4 py-10 grid grid-cols-1 md:grid-cols-4 gap-8">
         <div>
         <img
